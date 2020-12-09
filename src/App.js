6d3b11352e3b7bbb4ba8b5c451893affde9b1ba2
Toby Boudreaux <tobynboudreaux@gmail.com>
2020-12-09 14:02:07 -0600

    add-page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import HomePage from './components/pages/HomePage';
+import { AddPage } from './components/pages/AddPage';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={HomePage}/>
-        <Route path="/" render={()=> <div>404 NOT FOUND :)</div>}/>       
+        <Route path="/add" component={AddPage} />  
+        <Route path="/" render={()=> <div>404 NOT FOUND :)</div>}/>   
       </Switch>
     </Router>
   );
