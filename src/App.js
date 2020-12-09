78150b4c10fcc2df80948942a31527cbc1dace2f
dansirdan <daniel.monteton@gmail.com>
2020-12-09 09:00:40 -0800

    Added react-router-dom to npm dependecies / created routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import HomePage from './components/pages/HomePage';
 
 function App() {
   return (
-    <HomePage/>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route path="/" render={()=> <div>404 NOT FOUND :)</div>}/>       
+      </Switch>
+    </Router>
   );
 }
 
